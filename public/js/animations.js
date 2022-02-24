794fb6f5610117d6bb4f85a6461fb6812a8b2efc
Camilo Mendoza <camilomendoza804@gmail.com>
2022-02-23 21:14:32 -0400

    f -Se anadio boton Paypal,tambien se adapto para todo tamano de dispositivo y mejorar header

---
+++ public/js/animations.js
@@ -0,0 +1,39 @@
+const mainTitle = document.getElementById("mainTitle");
+const navToggle = document.querySelector(".menu-bar__toggle");
+const navMenu = document.querySelector(".nav-bar__links");
+mainTitle.addEventListener("click", function () {
+  Swal.fire({
+    text: "",
+    html: "<b class='text-alert'>Estas a punto de recargar la pagina</b>",
+    icon: "warning",
+    showDenyButton: true,
+    confirmButtonText: `Continuar`,
+    denyButtonText: `Cancelar`,
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      window.location.reload();
+    }
+  });
+});
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-bar__visible");
+});
+//animateShadow(mainTitle);
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function animateShadow(objectDOM) {
+  c = 3;
+  while (c < 100) {
+    //console.log(c);
+    objectDOM.style.textShadow = `2px 2px ${c}px #cfd6cf`;
+    await sleep(60);
+    c++;
+    if (c == 100) {
+      c = 3;
+    }
+  }
+}
--- public/js/share.js
+++ public/js/share.js
@@ -2,29 +2,48 @@ const shareWhatsappButton = document.getElementById("shareWhatsappButton");
 shareWhatsappButton.addEventListener("click", shareMessageByWhatsapp);
 
 function shareMessageByWhatsapp() {
+  //msgByUser = prompt("Añada su mensaje");
   var msgToshare = "";
-  console.log("Enviando mensaje wps");
+  console.log("Enviando mensaje wsp");
   if (listaContadores.length == 1) {
-    msgToshare =
-      "https://wa.me/?text=En%20la%20reunion:%20*" +
-      document.getElementById("iM0").value +
-      "*%20tenia%20*" +
-      listaContadores[0].counter +
-      "*%20personas";
+    if (document.getElementById("iM0").value == "") {
+      msgToshare =
+        "https://wa.me/?text=A%20la%20reunion:%20*sin%20nombre*%20tenia%20*" +
+        listaContadores[0].counter +
+        "*%20personas";
+    } else {
+      msgToshare =
+        "https://wa.me/?text=En%20la%20reunion:%20*" +
+        document.getElementById("iM0").value +
+        "*%20tenia%20*" +
+        listaContadores[0].counter +
+        "*%20personas";
+    }
   } else {
     var idInput = "iM";
     var msgToAssemble = "";
     msgToshare = "https://wa.me/?text=A%20las%20reuniones:%20";
     for (let i = 0; i < listaContadores.length; i++) {
       console.log(document.getElementById(idInput + i).value);
-      msgToAssemble =
-        "%0A-%20*" +
-        document.getElementById(idInput + i).value +
-        "*%20asistieron%20*" +
-        listaContadores[i].counter +
-        "*%20personas";
+      msgToAssemble = getMsgToAsm();
+      if (document.getElementById(idInput + i).value == "") {
+        msgToAssemble =
+          "%0A-%20*sin%20nombre*%20asistieron%20*" +
+          listaContadores[i].counter +
+          "*%20personas";
+      } else {
+        msgToAssemble =
+          "%0A-%20*" +
+          document.getElementById(idInput + i).value +
+          "*%20asistieron%20*" +
+          listaContadores[i].counter +
+          "*%20personas";
+      }
+
       msgToshare = msgToshare + msgToAssemble;
     }
   }
   window.open(msgToshare, "_blank");
 }
+
+function getMsgToAsm(element) {}
