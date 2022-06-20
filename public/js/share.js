55567dac6ddf48c4ac58e9a70a3ae83ba51fc22f
Camilo Mendoza <camilomendoza804@gmail.com>
2022-06-20 13:56:05 -0400

    -f Quitar espacios a nombres de contadores para compartir por WSP

--- public/js/share.js
+++ public/js/share.js
@@ -4,37 +4,42 @@ shareWhatsappButton.addEventListener("click", shareMessageByWhatsapp);
 function shareMessageByWhatsapp() {
   //msgByUser = prompt("Añada su mensaje");
   var msgToshare = "";
+  var counterName = "";
   console.log("Enviando mensaje wsp");
   if (listaContadores.length == 1) {
-    if (document.getElementById("iM0").value == "") {
+    counterName = deleteSpacesIfHaveIt(document.getElementById("iM0").value);
+    if (counterName == "") {
       msgToshare =
-        "https://wa.me/?text=A%20la%20reunion:%20*sin%20nombre*%20tenia%20*" +
+        "https://wa.me/?text=En%20la%20reunion:%20*sin%20nombre*%20habia%20*" +
         listaContadores[0].counter +
         "*%20personas";
     } else {
       msgToshare =
         "https://wa.me/?text=En%20la%20reunion:%20*" +
-        document.getElementById("iM0").value +
-        "*%20tenia%20*" +
+        counterName +
+        "*%20habia%20*" +
         listaContadores[0].counter +
         "*%20personas";
     }
   } else {
     var idInput = "iM";
     var msgToAssemble = "";
-    msgToshare = "https://wa.me/?text=A%20las%20reuniones:%20";
+    msgToshare = "https://wa.me/?text=En%20las%20reuniones:%20";
     for (let i = 0; i < listaContadores.length; i++) {
-      console.log(document.getElementById(idInput + i).value);
-      msgToAssemble = getMsgToAsm();
-      if (document.getElementById(idInput + i).value == "") {
+      counterName = document.getElementById(idInput + i).value;
+      console.log(counterName);
+      if (counterName == "") {
         msgToAssemble =
           "%0A-%20*sin%20nombre*%20asistieron%20*" +
           listaContadores[i].counter +
           "*%20personas";
       } else {
+        counterName = deleteSpacesIfHaveIt(
+          document.getElementById(idInput + i).value
+        );
         msgToAssemble =
           "%0A-%20*" +
-          document.getElementById(idInput + i).value +
+          counterName +
           "*%20asistieron%20*" +
           listaContadores[i].counter +
           "*%20personas";
@@ -44,4 +49,12 @@ function shareMessageByWhatsapp() {
     }
   }
   window.open(msgToshare, "_blank");
+}
+
+function deleteSpacesIfHaveIt(text) {
+  var newText = text;
+  if (text[text.length - 1] == " ") {
+    newText = text.slice(0, -1);
+  }
+  return newText;
 }
